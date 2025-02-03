07f3e82f4ab67d45c77d99a1e4d4d70850d374c3
Diana Papp <pappdiana148@gmail.com>
2025-02-03 11:58:42 +0200

    Implement TodoList and TodoItem with TypeScript function declarations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import Todos from './components/Todos';
+import Todo from './util/todo';
+
+
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ]
 
   return (
     <>
       <div>
-        
+        <Todos items={todos}/>
       </div>
     </>
   )
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NewTodo : React.FC = () => {
+    return(
+        <form>
+            <label htmlFor="text">Todo text</label>
+            <input type="text" id="text"/>
+            <button>Add Todo</button>
+        </form>
+    )
+}
+
+export default NewTodo
